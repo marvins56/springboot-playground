e15fe3cb18393e55781bad2d9ff5aa7bbe6652a8
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-08 13:51:29 +0300

    🐱 created  saveDepartment

--- src/main/java/com/example/practise/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/practise/service/DepartmentServiceImpl.java
@@ -1,8 +1,19 @@
 package com.example.practise.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.practise.entity.Department;
+import com.example.practise.repository.DepartmentRepository;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService  {
+@Autowired
+	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		
+		return departmentRepository.save(department);
+	}
 
 }
